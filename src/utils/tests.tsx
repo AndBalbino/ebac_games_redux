c5a1858ee204085403276af6449ae1fae055ff3c
Anderson Balbino <andersonbalbino2212@outlook.com>
2024-10-07 21:13:19 -0300

    Criando os testes

--- src/utils/tests.tsx
+++ src/utils/tests.tsx
@@ -1,11 +1,33 @@
-import {PreloadedState} from '@reduxjs/toolkit'
-import { RenderOptions } from '@testing-library/react'
+import { PreloadedState } from '@reduxjs/toolkit'
+import { RenderOptions, render } from '@testing-library/react'
 
-type ExtendedRenderOptions = Omit<RenderOptions, 'queries'> {
-  preloadedState?: PreloadedState
+import { AppStore, RootState, configuraStore } from '../store'
+import { PropsWithChildren } from 'react'
+import { Provider } from 'react-redux'
+
+interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
+  preloadedState?: PreloadedState<RootState>
+  store?: AppStore
 }
 
 export function renderizaComProvider(
   elemento: React.ReactElement,
-  { preloadeState = {}, store, ...opcoesAdicionais }: ExtendedRenderOptions = {}
-)
+  {
+    preloadedState = {},
+    store = configuraStore(preloadedState),
+    ...opcoesAdicionais
+  }: ExtendedRenderOptions = {}
+) {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  function Encapsulador({ children }: PropsWithChildren<{}>): JSX.Element {
+    return <Provider store={store}>{children}</Provider>
+  }
+
+  return {
+    store,
+    ...render(elemento, {
+      wrapper: Encapsulador,
+      ...opcoesAdicionais
+    })
+  }
+}
